7c8647f9fa406d63bf13f68b331ff64a4e55842c
horangeee <anbanghorangee@gmail.com>
2020-05-24 00:40:13 +0900

    initial commit

---
+++ alfred-admin-dashboard/src/test/java/io/alfred/admin/AlfredAdminDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.alfred.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfredAdminDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
